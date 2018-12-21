325f5bc10ac5d191521faa6a0324e7f9a0f8c370
Marcos Pivetta <marcos.pivetta@hotmail.com.br>
2018-12-21 19:32:21 -0200

    Removendo registro (CRUD)

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -63,6 +63,12 @@ public class LivroBean {
 
 		this.livro = new Livro();
 	}
+	
+	public void remover(Livro livro) {
+		System.out.println("Removendo livro");
+		
+		new DAO<Livro>(Livro.class).remove(livro);
+	}
 
 	public String formAutor() {
 		System.out.println("Chamando o formulário do Autor.");

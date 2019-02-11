dc4395bf871b01b1e63454f76c251f9c3d790403
Marcos Pivetta <marcos.pivetta@hotmail.com.br>
2019-02-10 23:23:00 -0200

    Oferecendo todos os temas

--- src/br/com/caelum/livraria/bean/TemaBean.java
+++ src/br/com/caelum/livraria/bean/TemaBean.java
@@ -17,4 +17,15 @@ public class TemaBean {
 		this.tema = tema;
 	}
 	
+	public String[] getTemas() {
+		return new String[] { "afterdark", "afternoon", "afterwork", "aristo",
+	            "black-tie", "blitzer", "bluesky", "bootstrap", "casablanca",
+	            "cupertino", "cruze", "dark-hive", "delta", "dot-luv",
+	            "eggplant", "excite-bike", "flick", "glass-x", "home",
+	            "hot-sneaks", "humanity", "le-frog", "midnight", "mint-choc",
+	            "overcast", "pepper-grinder", "redmond", "rocket", "sam",
+	            "smoothness", "south-street", "start", "sunny", "swanky-purse",
+	            "trontastic", "ui-darkness", "ui-lightness", "vader" };
+	}
+	
 }

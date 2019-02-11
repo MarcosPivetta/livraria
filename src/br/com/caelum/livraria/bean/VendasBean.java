9ad9230a157b0722954b699ab8641ed6280ad55e
Marcos Pivetta <marcos.pivetta@hotmail.com.br>
2019-02-10 23:59:48 -0200

    A página de vendas

--- src/br/com/caelum/livraria/bean/VendasBean.java
+++ src/br/com/caelum/livraria/bean/VendasBean.java
@@ -7,30 +7,51 @@ import java.util.Random;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Livro;
 import br.com.caelum.livraria.modelo.Venda;
 
 @ManagedBean
 @ViewScoped
-public class VendaBean {
+public class VendasBean {
+
+	public BarChartModel getVendasModel() {
+
+		BarChartModel model = new BarChartModel();
+
+		ChartSeries vendaSerie = new ChartSeries();
+		vendaSerie.setLabel("Vendas 2016");
+
+		List<Venda> vendas = getVendas();
+
+		for (Venda venda : vendas) {
+			vendaSerie.set(venda.getLivro().getTitulo(), venda.getQuantidade());
+		}
+
+		model.addSeries(vendaSerie);
+
+		return model;
+
+	}
 
 	public List<Venda> getVendas() {
-		
+
 		List<Venda> vendas = new ArrayList<Venda>();
 		List<Livro> livros = new DAO<Livro>(Livro.class).listaTodos();
 
+		Random random = new Random(1234);
+		
 		for (Livro livro : livros) {
-			
-			Random random = new Random(1234);
 			Integer quantidade = random.nextInt(500);
-			
 			vendas.add(new Venda(livro, quantidade));
-			
+
 		}
-		
+
 		return vendas;
-		
+
 	}
-	
+
 }

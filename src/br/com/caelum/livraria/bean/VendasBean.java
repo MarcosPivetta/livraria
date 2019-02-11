d4256a64cb5e31227e64df6b39c64b0d5534e3fe
Marcos Pivetta <marcos.pivetta@hotmail.com.br>
2019-02-11 00:11:41 -0200

    Terminando o projeto

--- src/br/com/caelum/livraria/bean/VendasBean.java
+++ src/br/com/caelum/livraria/bean/VendasBean.java
@@ -21,28 +21,39 @@ public class VendasBean {
 	public BarChartModel getVendasModel() {
 
 		BarChartModel model = new BarChartModel();
+				model.setAnimate(true);
 
 		ChartSeries vendaSerie = new ChartSeries();
 		vendaSerie.setLabel("Vendas 2016");
 
-		List<Venda> vendas = getVendas();
+		List<Venda> vendas = getVendas(1234);
 
 		for (Venda venda : vendas) {
 			vendaSerie.set(venda.getLivro().getTitulo(), venda.getQuantidade());
 		}
+		
+		ChartSeries vendaSerie2015 = new ChartSeries();
+		vendaSerie2015.setLabel("Vendas 2015");
 
+		vendas = getVendas(4321);
+		for (Venda venda : vendas) {
+			vendaSerie2015.set(venda.getLivro().getTitulo(), venda.getQuantidade());
+		}
+		
+		
 		model.addSeries(vendaSerie);
+		model.addSeries(vendaSerie2015);
 
 		return model;
 
 	}
 
-	public List<Venda> getVendas() {
+	public List<Venda> getVendas(long seed) {
 
 		List<Venda> vendas = new ArrayList<Venda>();
 		List<Livro> livros = new DAO<Livro>(Livro.class).listaTodos();
 
-		Random random = new Random(1234);
+		Random random = new Random(seed);
 		
 		for (Livro livro : livros) {
 			Integer quantidade = random.nextInt(500);
